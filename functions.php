ad568b2b1192e8d385c50553653d272a8e0d2e48
MarianMaschkeRGST <m.maschke@rgst.net>
2022-05-25 19:04:50 +0900

    added sample contents

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php 
+
+function load_css() {
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('main', get_theme_file_uri('/css/main.css'));
+
+};
+
+add_action('wp_enqueue_scripts', 'load_css');
